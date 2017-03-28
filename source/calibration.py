4d52d767ee0ec81bfd5cf7a6d049e37736a6af2b
Cédric Bodet <cedric.bodet@tngtech.com>
2017-03-28 15:08:17 +0200

    Created Calibration class

--- source/calibration.py
+++ source/calibration.py
@@ -1,50 +1,65 @@
 import numpy as np
 import cv2
-
-def find_points( images ):
-    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-    objp = np.zeros((6 * 9, 3), np.float32)
-    objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
-
-    # Arrays to store object points and image points from all the images.
-    objpoints = []  # 3d points in real world space
-    imgpoints = []  # 2d points in image plane.
-
-    # Step through the list and search for chessboard corners
-    for idx, fname in enumerate(images):
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-        # Find the chessboard corners
-        ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
-
-        # If found, add object points, image points
-        if ret == True:
-            objpoints.append(objp)
-            imgpoints.append(corners)
-
-            # Draw and display the corners
-            cv2.drawChessboardCorners(img, (9, 6), corners, ret)
-            # write_name = 'corners_found'+str(idx)+'.jpg'
-            # cv2.imwrite(write_name, img)
-            cv2.imshow('img', img)
-            cv2.waitKey(500)
-        else:
-            print("No corners found for image ", fname)
-
-    cv2.destroyAllWindows()
-
-    return objpoints, imgpoints
+import pickle
 
 
-def calibrate_camera(calibration_images):
-    objpoints, imgpoints = find_points(calibration_images)
-    img_size = (1280, 720)
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+class Calibration:
+    """A class for transforming the """
+    mtx = None
+    dist = None
 
-    return mtx, dist
+    def __init__(self, images=None, pickle_file=None):
+        self.mtx = []
+        self.dist = []
 
-def undistort(img, mtx, dist):
-    return cv2.undistort(img, mtx, dist, None, mtx)
+        if images is None and pickle_file is None:
+            raise Exception("Please provide either image glob or pickle file!")
 
+        if images is not None:
+            self.calibrate_camera(images)
 
+        if pickle_file is not None:
+            self.from_pickle(pickle_file)
+
+    def calibrate_camera(self, images):
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((6 * 9, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+        # Arrays to store object points and image points from all the images.
+        objpoints = []  # 3d points in real world space
+        imgpoints = []  # 2d points in image plane.
+
+        # Step through the list and search for chessboard corners
+        for idx, fname in enumerate(images):
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+            # Find the chessboard corners
+            ret, corners = cv2.findChessboardCorners(gray, (9, 6), None)
+
+            # If found, add object points, image points
+            if ret == True:
+                objpoints.append(objp)
+                imgpoints.append(corners)
+            else:
+                print("No corners found for image ", fname)
+
+        img_size = (1280, 720)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
+        self.mtx = mtx
+        self.dist = dist
+
+    def from_pickle(self, pickle_file):
+        dist_pickle = pickle.load(open(pickle_file, "rb"))
+        self.mtx = dist_pickle["mtx"]
+        self.dist = dist_pickle["dist"]
+
+    def save_pickle(self, pickle_file):
+        dist_pickle = {}
+        dist_pickle["mtx"] = self.mtx
+        dist_pickle["dist"] = self.dist
+        pickle.dump(dist_pickle, open(pickle_file, "wb"))
+
+    def undistort(self, img):
+        return cv2.undistort(img, self.mtx, self.dist, None, self.mtx)
--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -2,15 +2,16 @@ import glob
 import cv2
 import matplotlib.pyplot as plt
 
-from source import calibration
+from source.calibration import Calibration
 
 # Make a list of calibration images
 images = glob.glob('camera_cal/calibration*.jpg')
 
-mtx, dist = calibration.calibrate_camera(images)
+calibration = Calibration(images=images)
+calibration.save_pickle('camera_cal/cam_dist_pickle.p')
 
 img = cv2.imread('camera_cal/calibration1.jpg')
-dst = calibration.undistort(img, mtx, dist)
+dst = calibration.undistort(img)
 
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
 ax1.imshow(img)
@@ -20,7 +21,10 @@ ax2.set_title('Undistorted Image', fontsize=30)
 
 plt.show()
 
+img = cv2.imread('tests/snapshot.png')
+dst = calibration.undistort(img)
+cv2.imwrite('tests/calibration/snapshot.png', dst)
+
 from source.video import process_video
 
-process_video('project_video.mp4', 'tests/calibration/project_video.mp4',
-              lambda image: calibration.undistort(image, mtx, dist))
+process_video('project_video.mp4', 'tests/calibration/project_video.mp4', calibration.undistort)

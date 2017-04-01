0168f2678973fbf9504a4a73d51955609760065e
Cédric Bodet <cedric.bodet@tngtech.com>
2017-04-01 20:44:52 +0200

    Code cleaunp

--- tests/test_calibration.py
+++ tests/test_calibration.py
@@ -3,6 +3,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 from source.calibration import Calibration
+from source import util
 
 # Make a list of calibration images
 images = glob.glob('camera_cal/calibration*.jpg')
@@ -10,20 +11,20 @@ images = glob.glob('camera_cal/calibration*.jpg')
 calibration = Calibration(images=images)
 calibration.save_pickle('camera_cal/cam_dist_pickle.p')
 
-img = cv2.imread('camera_cal/calibration1.jpg')
-dst = calibration.undistort(img)
+# Step through the list and search for chessboard corners
+for idx, fname in enumerate(images):
+    img = cv2.imread(fname)
+    dst = calibration.undistort(img)
 
-f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
-ax1.imshow(img)
-ax1.set_title('Original Image', fontsize=30)
-ax2.imshow(dst)
-ax2.set_title('Undistorted Image', fontsize=30)
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+    ax1.imshow(img)
+    ax1.set_title('Original Image', fontsize=30)
+    ax2.imshow(dst)
+    ax2.set_title('Undistorted Image', fontsize=30)
 
-plt.show()
+    plt.show()
 
-img = cv2.imread('tests/snapshot.png')
-dst = calibration.undistort(img)
-cv2.imwrite('tests/calibration/snapshot.png', dst)
+    cv2.imwrite('tests/calibration/' + util.get_filename(fname), dst)
 
 from source.video import process_video
 
